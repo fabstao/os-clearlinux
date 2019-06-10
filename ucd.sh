0f4fe22cbc2e739887d054d1fc84e61b6aa7cc11
fsalam1 <fabian.salamanca@intel.com>
2019-06-10 14:16:16 +0000

    Tested version for legacy BIOS

--- ucd.sh
+++ ucd.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-<<<<<<< HEAD
 main() {
 	/usr/bin/systemctl enable ucd.service
-=======
-
-DIR=/usr/lib/systemd/system/multi-user.target.wants/
-
-main() {
-	ln -s /usr/lib/systemd/system/ucd.service ${DIR}
->>>>>>> 80b95944a1b81c0f5d7a7afa1c05d13437bfbafc
 }
 
 main $@
